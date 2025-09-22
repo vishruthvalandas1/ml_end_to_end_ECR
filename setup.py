f2ec604cfb1c7fd0dc0075ad3a8f15226ffdfd56
vishruthvalandas1 <vishruth.valandas@gmail.com>
2025-09-22 15:49:48 -0500

    addition of setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+def get_requirements(file_path):
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='vishruth',
+    author_email='vishu.valandas@gmail.com',
+    description='ML Project Template',
+    url='',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
