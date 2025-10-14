c8465b826fc7a01ba9d1e0308844de516777fce9
vishruthvalandas08 <vishruth.valandasus@gmail.com>
2025-10-14 17:22:44 -0500

    change i port

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def predict_datapoint():
             return render_template('home.html', results=error_msg)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000)
+    app.run(host="0.0.0.0",port=8080)
